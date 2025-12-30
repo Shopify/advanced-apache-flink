34bf834ccea5bd2b81b23aa2207dfd390c2da558
Yaroslav Tkachenko <sapiensy@gmail.com>
2025-12-29 16:57:26 -0800

    chore: add hints

--- table/src/main/java/dev/irontools/flink/XxHashFunction.java
+++ table/src/main/java/dev/irontools/flink/XxHashFunction.java
@@ -1,8 +1,12 @@
 package dev.irontools.flink;
 
 import net.openhft.hashing.LongTupleHashFunction;
+import org.apache.flink.table.annotation.DataTypeHint;
+import org.apache.flink.table.annotation.FunctionHint;
 import org.apache.flink.table.functions.ScalarFunction;
 
+// @FunctionHint(input = {@DataTypeHint("STRING")})
+// @FunctionHint(input = {@DataTypeHint("BOOLEAN")})
 public class XxHashFunction extends ScalarFunction {
   public String eval(String string) {
     long[] hash = LongTupleHashFunction.xx128().hashChars(string);
